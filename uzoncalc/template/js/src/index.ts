cf4c218de354f8780f784c75c576d324c74abab7
galens <gmx_galens@163.com>
2026-04-22 13:43:11 +0800

    feat: 添加滚动记忆功能，保存和恢复页面滚动位置

--- uzoncalc/template/js/src/index.ts
+++ uzoncalc/template/js/src/index.ts
@@ -1,9 +1,11 @@
+import { setupScrollMemory } from "./scrollMemory";
 import { ensureTemplateStyles } from "./styleInjector";
 import { generateToc } from "./toc";
 
 function bootstrap(): void {
   ensureTemplateStyles();
   generateToc();
+  setupScrollMemory();
 }
 
 ensureTemplateStyles();
