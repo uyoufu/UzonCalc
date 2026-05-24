26a7eec898315b04742be8b0ed86a2890aef9380
galens <gmx_galens@163.com>
2026-05-24 22:00:29 +0800

    feat: 新增计算书模板打印按钮功能

--- src/core/uzoncalc/template/js/src/styles/index.ts
+++ src/core/uzoncalc/template/js/src/styles/index.ts
@@ -4,6 +4,7 @@ import baseStyles from "./base.css" with { type: "text" };
 import codeStyles from "./code.css" with { type: "text" };
 import interactiveStyles from "./interactive.css" with { type: "text" };
 import mathStyles from "./math.css" with { type: "text" };
+import printBtnStyles from "./printBtn.css" with { type: "text" };
 import printStyles from "./print.css" with { type: "text" };
 import tableStyles from "./table.css" with { type: "text" };
 import tocStyles from "./toc.css" with { type: "text" };
@@ -17,5 +18,6 @@ export const templateStyles = [
   codeStyles,
   tableStyles,
   tocStyles,
+  printBtnStyles,
   printStyles,
 ].join("\n\n");
